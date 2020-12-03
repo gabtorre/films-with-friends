466eee5ff2981f71f160ca7921ac597c2ca4b9a0
gabtorre <gtorresx3@gmail.com>
2020-12-03 12:52:49 -0800

    Initialize react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import { ApolloProvider, useQuery, useMutation } from '@apollo/react-hooks'
+
+
+const client = new ApolloClient({
+  uri: 'https://workers-graphql-server.gabtorres.workers.dev/',
+})
+
+const GET_POSTS = gql`
+  {
+    post {
+      id
+      text
+    }
+  }
+`
+
+const Posts = () => {
+  const { loading, error, data } = useQuery(GET_POSTS)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :</p>
+
+  return (
+    <>
+        {data.post.map( ({ text }) => (
+          <h3>{text}</h3>
+        ))}
+    </>
+  );
+}
+
+function AddPost() {
+  let input
+  const [createPost] = useMutation(
+    gql `
+      mutation createPost($text: String!) {
+        createPost(text: $text){
+          id
+          text
+        }
+      }
+    `,
+    {
+      update(
+        cache,
+        {
+          data: { createPost },
+        },
+      ) {
+        const { post } = cache.readQuery({query: GET_POSTS})
+        cache.writeQuery({
+          query: GET_POSTS,
+          data: { post: [createPost].concat(post)}
+        })
+      }
+    }
+  )
+
+  return (
+    <>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          createPost({ variables: { text: input.value } })
+          input.value = ''
+        }}
+      >
+        <input
+          ref={node => {
+            input = node
+          }}
+        />
+        <button type="submit">Add Post</button>
+      </form>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Posts/>
+      <AddPost/>
+    </ApolloProvider>
   );
 }
 
