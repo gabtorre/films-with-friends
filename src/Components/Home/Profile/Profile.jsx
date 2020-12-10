46991aca18a7bd08467e495c7d5b14fd66f5e219
gabtorre <gtorresx3@gmail.com>
2020-12-10 12:31:33 -0800

    Fix Horizontal Scroll

--- src/Components/Home/Profile/Profile.jsx
+++ src/Components/Home/Profile/Profile.jsx
@@ -1,14 +1,25 @@
 import { WatchList } from '../Profile/Watchlist';
 import { WatchedList } from '../Profile/WatchedList';
 import { ActivityPostsWrapper }  from '../../StyledComponents'
+import styled from 'styled-components';
+import {Container} from 'react-bootstrap';
 
 const Profile = (props) => {
 
+    const WatchWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    `
+
     return (
-        <ActivityPostsWrapper>
+        <WatchWrapper>
+            <Container fluid>
             <WatchList user={props.uid}/>
             <WatchedList user={props.uid}/>
-        </ActivityPostsWrapper>
+            </Container>
+        </WatchWrapper>
     );
 }
 
--- src/Components/Home/Profile/WatchedList.jsx
+++ src/Components/Home/Profile/WatchedList.jsx
@@ -4,49 +4,29 @@ import 'firebase/firestore';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import styled from 'styled-components';
 import HorizontalScroll from 'react-scroll-horizontal';
+import Row from 'react-bootstrap/Row';
+import ScrollHorizontal from 'react-scroll-horizontal';
+import {MoviePoster, MovieTitle, ChildDiv} from '../../../Components/StyledComponents';
 
 export const WatchedList = (props) => {
-
-    const MoviePoster = styled.img`
-    width: 150px;
-    border-radius: 10%;
-    `
-    const MovieTitle = styled.p`
-    text-align: center;
-    font-weight: 700;
-    line-height: 1em;
-    padding: 10px 0;
-    `
-    const ListContainer = styled.div`
-    height: 22.7em;
-    `
-    const ParentDiv = styled.div`
-    width: 50em;
-    height: 100%;
-    `
-    const ChildDiv = styled.div`
-    width: 150px;
-    height: 100%;
-    margin: 10px;
-    `
     
     const firestore = firebase.firestore();
     const list = firestore.collection('watched').where('uid', '==', props.user)
     const [ userwatchedlist ] = useCollectionData(list, {idField: 'uid'});
 
     return (
-        <ListContainer>
-            <h1 className="mt-5 mb-4 bold">Watch List</h1>
-            <ParentDiv>
-                <HorizontalScroll>
+        <> 
+            <h1 className="mt-5 mb-4 bold">Watched List</h1>
+            <div style={{ height: `22.7em` }}>
+                <ScrollHorizontal>
                     {userwatchedlist && userwatchedlist.map(movie =>
                     <ChildDiv>
                         <MoviePoster variant="top" src={movie.poster} />
                         <MovieTitle key={movie.id}>{movie.title}</MovieTitle>
                     </ChildDiv>
                     )}
-                </HorizontalScroll>
-            </ParentDiv>
-        </ListContainer>
+                </ScrollHorizontal>
+            </div>
+        </>
     );
 };
