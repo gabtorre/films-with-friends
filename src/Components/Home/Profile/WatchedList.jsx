154a0012d24d1238804636344e78c764f1747fd1
gabtorre <gtorresx3@gmail.com>
2020-12-10 14:55:54 -0800

    Update Watchlist data source

--- src/Components/Home/Profile/WatchedList.jsx
+++ src/Components/Home/Profile/WatchedList.jsx
@@ -1,25 +1,24 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import styled from 'styled-components';
-import HorizontalScroll from 'react-scroll-horizontal';
-import Row from 'react-bootstrap/Row';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
 import ScrollHorizontal from 'react-scroll-horizontal';
 import {MoviePoster, MovieTitle, ChildDiv} from '../../../Components/StyledComponents';
 
 export const WatchedList = (props) => {
     
     const firestore = firebase.firestore();
-    const list = firestore.collection('watched').where('uid', '==', props.user)
-    const [ userwatchedlist ] = useCollectionData(list, {idField: 'uid'});
+
+    const [userdata, loading, error] = useDocumentData(
+        firestore.doc('users/' + props.user)
+    );
 
     return (
         <> 
             <h1 className="mt-5 mb-4 bold">Watched List</h1>
             <div style={{ height: `22.7em` }}>
                 <ScrollHorizontal>
-                    {userwatchedlist && userwatchedlist.map(movie =>
+                    {userdata && userdata.watched.map(movie =>
                     <ChildDiv>
                         <MoviePoster variant="top" src={movie.poster} />
                         <MovieTitle key={movie.id}>{movie.title}</MovieTitle>
