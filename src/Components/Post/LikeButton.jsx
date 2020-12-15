3e23009f68885ae8358f02acd79dd09849dc9be2
Beatrix Woo <beatrixwho@gmail.com>
2020-12-15 10:19:47 -0800

    cleaned up

---
+++ src/Components/Post/LikeButton.jsx
@@ -0,0 +1,46 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { ReactComponent as FavoriteIcon } from "../../../Icons/Favorite.svg";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+
+const LikeButton = (props) => {
+    const firestore = firebase.firestore();
+    const post = firestore.collection("post").doc(props.id)
+
+    const [postData] = useDocumentData(post)
+
+    // console.log(postData, props.id)
+
+    const handleLike = async (e) => {
+        e.preventDefault();
+
+        await post.update({
+            likes: firebase.firestore.FieldValue.arrayUnion(props.uid)
+            })
+    }
+
+    return (
+        <>
+        { postData && postData.likes.length > 0 ?
+            <OverlayTrigger
+            key={props.key+"icon3"}
+            placement="top"
+            overlay={<Tooltip id={`tooltip-top`}>{postData.likes.length} Like</Tooltip>}
+          >
+              <FavoriteIcon className="post__icons" onClick={handleLike}/>
+          </OverlayTrigger>
+          : <OverlayTrigger
+          key={props.key+"icon3"}
+          placement="top"
+          overlay={<Tooltip id={`tooltip-top`}>0 Likes</Tooltip>}
+        >
+            <FavoriteIcon className="post__icons" onClick={handleLike} key={props.key}/>
+        </OverlayTrigger>
+    }
+    </>
+    );
+}
+
+export default LikeButton;
---
+++ src/Components/Post/Posts.jsx
@@ -0,0 +1,37 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import Post from './Post';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Card } from 'react-bootstrap';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const Posts = (props) => {
+    // console.log(props)
+    const firestore = firebase.firestore();
+    const postRef = firestore.collection('posts');
+    const sortedPostsRef = postRef.orderBy('createdAt', 'desc');
+    const [ sortedposts ] = useCollectionData(sortedPostsRef, {idField: 'id'});
+
+    return (
+        <>
+        <h1 className="mt-5 mb-4 bold">Activity Feed</h1>
+        <Row>
+            {sortedposts && sortedposts.map(post =>
+                <Col xl={6}>
+                    <Card className="mb-5">
+                        <Post key={post.id} id={post.id} text={post.text}
+                        image={post.poster} title={post.title}
+                        synopsis={post.synopsis} release={post.release}
+                        uid={post.uid} username={post.username}
+                        findProfile={props.findProfile} createdAt={post.createdAt}
+                        photoURL={post.photoURL} rating={post.rating} movieid={post.movieid}/>
+                    </Card>
+                </Col>
+            )}
+        </Row>
+        </>
+    );
+}
+
+export default Posts;
