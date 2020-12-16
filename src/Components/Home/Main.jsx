940a4c97a6fbb2e69e75fef0e6f28a3d0a64510a
gabtorre <gtorresx3@gmail.com>
2020-12-16 12:13:49 -0800

    Update Friends Layout

--- src/Components/Home/Main.jsx
+++ src/Components/Home/Main.jsx
@@ -7,7 +7,7 @@ export default class Main extends Component {
   render() {
     return (
       <ActivityPostsWrapper className="scrollable-div">
-        <div style={{width: '50%', minWidth: '600px' , marginLeft: 'auto', marginRight: 'auto'}} >
+        <div style={{maxWidth: '600px', minWidth: '600px' , marginLeft: 'auto', marginRight: 'auto'}} >
             <Posts findProfile={this.props.findProfile} />
         </div>
       </ActivityPostsWrapper>
